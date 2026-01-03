054c918ea8d5a1adff3ed7fe3ba652d738ae6c67
tanneguydv <tanneguydv@gmail.com>
2026-01-03 23:47:25 +0000

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'tf_tree_terminal'
 
 setup(
     name=package_name,
-    version='1.0.1',
+    version='2.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
